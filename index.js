83de78f4bd4aa5a7c8e945097bf7e3b8c7eebdda
Otthon Leão <otthonleao@hotmail.com>
2022-11-13 17:51:03 -0400

    Criação de Endpoints para leitura invidual e de listas por meio de 'id'

--- index.js
+++ index.js
@@ -4,8 +4,36 @@ const port = 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World');
-})
+});
+
+/*
+Lista de Endpoints da aplicação CRUD de mensagens
+CRUD: Create, Read (single & all), Update and Delete
+- [GET] /mensagens - Retorna a lista de mensagens
+- [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
+- [POST] /mensagens - Cria uma nova mensagem
+- [PUT] /mensagens/{id} - Atualiza uma mensagem pelo ID
+- [DELETE] /mensagens/{id} - Remover uma mensagem pelo ID
+*/
+
+const mensagens = [
+	"Essa é a primeira mensagem",
+	"Essa é a segunda mensagem"
+]
+
+// - [GET] /mensagens - Retorna a lista de mensagens
+app.get("/mensagens", function (req, res) {
+	res.send(mensagens);
+	console.info(`Mensagens: \"${mensagens}\" foram retornadas pelo GET`)
+});
+
+// - [GET] /mensagens/{id} - Retorna apenas uma única mensagem pelo ID
+app.get("/mensagens/:id", function (req, res) {
+	const id = req.params.id - 1; //Subtrai 1 na requisição para ficar de acordo com o que o usuário quer na posição da lista que começa em 0. Então se o usuário quer a posição 2, na array vai ser 1.
+	const mensagem = mensagens[id];
+	res.send(mensagem);
+});
 
 app.listen(port, function () {
 	console.info(`App rodando em http://localhost:${port}`);
-})
+});
